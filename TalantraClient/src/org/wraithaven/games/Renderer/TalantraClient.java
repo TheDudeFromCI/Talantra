1cb2054deaecaaf2c31af6064a4f9ea6c06ee367
TheDudeFromCI <linktheblademaster@yahoo.com>
2016-01-04 06:23:53 -0700

    Played around with a few math classes.
    Signed-off-by: TheDudeFromCI <linktheblademaster@yahoo.com>

--- TalantraClient/src/org/wraithaven/games/Renderer/TalantraClient.java
+++ TalantraClient/src/org/wraithaven/games/Renderer/TalantraClient.java
@@ -1,27 +1,22 @@
 package org.wraithaven.games.Renderer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.nio.FloatBuffer;
-import java.nio.ShortBuffer;
 import java.text.NumberFormat;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11;
-import org.wraith.engine.gui.UserInterface;
-import org.wraith.engine.gui.UserInterfaceComponent;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
 import org.wraith.engine.loop.MainLoop;
 import org.wraith.engine.loop.RenderLoop;
 import org.wraith.engine.loop.WindowInitalizer;
 import org.wraith.engine.math.Matrix4f;
-import org.wraith.engine.rendering.Model;
-import org.wraith.engine.rendering.PrimitiveGenerator;
-import org.wraith.engine.rendering.ShaderProgram;
-import org.wraith.engine.rendering.ShaderProtocol;
-import org.wraith.engine.rendering.ShaderProtocolVariable;
-import org.wraith.engine.rendering.Texture;
+import org.wraith.engine.math.Vector3f;
 import org.wraith.engine.rendering.TimeKeeper;
-import org.wraith.engine.rendering.Universe;
-import org.wraith.engine.rendering.VertexBuildData;
 
 public class TalantraClient extends MainLoop implements RenderLoop{
 	public static void main(String[] args){
@@ -32,9 +27,6 @@ public class TalantraClient extends MainLoop implements RenderLoop{
 	}
 	private Input input;
 	private TimeKeeper timeKeeper;
-	private UserInterface ui;
-	private TimedPostProcessor processor;
-	private Universe universe;
 	private double lastFpsUpdate = -1;
 	public TalantraClient(){
 		input = new Input();
@@ -43,11 +35,12 @@ public class TalantraClient extends MainLoop implements RenderLoop{
 		return input;
 	}
 	public void render(){
-		processor.beginFrame();
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
-		universe.render();
-		ui.render();
-		processor.endFrame();
+		GL20.glEnableVertexAttribArray(0);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBuffer);
+		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 12, 0);
+		GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, 3);
+		GL20.glDisableVertexAttribArray(0);
 	}
 	public void update(double delta, double time){
 		timeKeeper.update(delta, time);
@@ -58,15 +51,59 @@ public class TalantraClient extends MainLoop implements RenderLoop{
 				"Talantra [Frametime: ~"+NumberFormat.getInstance().format(frameTime*1000)+"ms ("+Math.round(1/frameTime)+" Fps)]");
 		}
 		input.update(time);
-		universe.update();
-		processor.update(time);
 	}
 	@Override
 	protected void dispose(){
-		processor.dispose();
-		ui.dispose();
-		universe.dispose();
+		GL15.glDeleteBuffers(vertexBuffer);
+		GL30.glDeleteVertexArrays(vertexArrayId);
+		GL20.glDeleteProgram(programId);
 	}
+	private int programId;
+	private void loadShaders(String vertexShaderPath, String fragmentShaderPath){
+		String vertexShaderCode = readFile(vertexShaderPath);
+		int vertexShaderId = GL20.glCreateShader(GL20.GL_VERTEX_SHADER);
+		GL20.glShaderSource(vertexShaderId, vertexShaderCode);
+		GL20.glCompileShader(vertexShaderId);
+		int vsStatus = GL20.glGetShaderi(vertexShaderId, GL20.GL_COMPILE_STATUS);
+		if(vsStatus!=GL11.GL_TRUE)
+			throw new RuntimeException(GL20.glGetShaderInfoLog(vertexShaderId));
+		String fragmentShaderCode = readFile(fragmentShaderPath);
+		int fragmentShaderId = GL20.glCreateShader(GL20.GL_FRAGMENT_SHADER);
+		GL20.glShaderSource(fragmentShaderId, fragmentShaderCode);
+		GL20.glCompileShader(fragmentShaderId);
+		int fsStatus = GL20.glGetShaderi(fragmentShaderId, GL20.GL_COMPILE_STATUS);
+		if(fsStatus!=GL11.GL_TRUE)
+			throw new RuntimeException(GL20.glGetShaderInfoLog(fragmentShaderId));
+		programId = GL20.glCreateProgram();
+		GL20.glAttachShader(programId, vertexShaderId);
+		GL20.glAttachShader(programId, fragmentShaderId);
+		GL20.glLinkProgram(programId);
+		GL20.glValidateProgram(programId);
+		int pStatus = GL20.glGetProgrami(programId, GL20.GL_LINK_STATUS);
+		if(pStatus!=GL11.GL_TRUE)
+			throw new RuntimeException(GL20.glGetProgramInfoLog(programId));
+		GL20.glDetachShader(programId, vertexShaderId);
+		GL20.glDetachShader(programId, fragmentShaderId);
+		GL20.glDeleteShader(vertexShaderId);
+		GL20.glDeleteShader(fragmentShaderId);
+	}
+	private static String readFile(String path){
+		File file = new File(path);
+		try{
+			BufferedReader in = new BufferedReader(new FileReader(file));
+			String code = "";
+			String line;
+			while((line = in.readLine())!=null)
+				code += line+"\n";
+			in.close();
+			return code;
+		}catch(Exception exception){
+			exception.printStackTrace();
+		}
+		return null;
+	}
+	private int vertexBuffer;
+	private int vertexArrayId;
 	@Override
 	protected void preloop(){
 		// Initalize OpenGL
@@ -75,38 +112,30 @@ public class TalantraClient extends MainLoop implements RenderLoop{
 		GL11.glAlphaFunc(GL11.GL_GREATER, 0.0f);
 		GL11.glClearColor(0, 0, 0, 1);
 		setFpsCap(60);
-		// User Interface
-		ui = new UserInterface();
-		new UserInterfaceComponent(ui, new Texture(new File("/home/thedudefromci/Desktop/catgirl.jpg"), false), true);
-		// Other classes
-		{
-			File vertex = new File("/home/thedudefromci/Desktop/Shader.vert");
-			File frag = new File("/home/thedudefromci/Desktop/Shader.frag");
-			ShaderProgram shader = new ShaderProgram(vertex, null, frag);
-			shader.loadAttributes("att_position", "att_uv");
-			shader.loadUniforms("uni_time");
-			processor = new TimedPostProcessor(800, 600, shader);
-		}
+		//Time keeper
 		timeKeeper = new TimeKeeper();
-		universe = new Universe();
-		{
-			// Load cube.
-			VertexBuildData build = PrimitiveGenerator.generateBox(0.5f, 0.5f, 0.5f);
-			ModelShader s = new ModelShader();
-			s.loadUniforms("uni_modelView", "uni_projection");
-			ShaderProtocol protocol = new ShaderProtocol(s, new ShaderProtocolVariable[]{
-				new ShaderProtocolVariable(3, false, GL11.GL_FLOAT, "att_pos")
-			}, true);
-			FloatBuffer vertexData = BufferUtils.createFloatBuffer(build.getVertexLocations().length);
-			ShortBuffer indexData = BufferUtils.createShortBuffer(build.getIndexLocations().length);
-			vertexData.put(build.getVertexLocations());
-			indexData.put(build.getIndexLocations());
-			vertexData.flip();
-			indexData.flip();
-			Matrix4f per = Matrix4f.perspective(70, 4/3f, 0.1f, 100.0f);
-			s.setUniformMat4(1, per.getBuffer());
-			Matrix4f mat = Matrix4f.translate(0, 0, -5);
-			universe.addModel(new Model(protocol, vertexData, indexData, mat));
-		}
+		//Build cube.
+		vertexArrayId = GL30.glGenVertexArrays();
+		GL30.glBindVertexArray(vertexArrayId);
+		FloatBuffer vertex_buffer_data = BufferUtils.createFloatBuffer(9);
+		vertex_buffer_data.put(-1.0f).put(-1.0f).put(0.0f);
+		vertex_buffer_data.put(1.0f).put(-1.0f).put(0.0f);
+		vertex_buffer_data.put(0.0f).put(1.0f).put(0.0f);
+		vertex_buffer_data.flip();
+		vertexBuffer = GL15.glGenBuffers();
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vertexBuffer);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertex_buffer_data, GL15.GL_STATIC_DRAW);
+		//Load shader.
+		loadShaders("C:/Users/TheDudeFromCI/Desktop/Vertex.txt", "C:/Users/TheDudeFromCI/Desktop/Fragment.txt");
+		GL20.glUseProgram(programId);
+		handleMatrixMath();
+	}
+	private void handleMatrixMath(){
+		Matrix4f projection = Matrix4f.perspective(45, 4/3f, 0.1f, 100.0f);
+		Matrix4f view = Matrix4f.lookAt(new Vector3f(4, 3, 3), new Vector3f(0, 0, 0), new Vector3f(0, 1, 0));
+		Matrix4f model = new Matrix4f();
+		GL20.glUniformMatrix4fv(GL20.glGetUniformLocation(programId, "projection"), false, projection.getBuffer());
+		GL20.glUniformMatrix4fv(GL20.glGetUniformLocation(programId, "view"), false, view.getBuffer());
+		GL20.glUniformMatrix4fv(GL20.glGetUniformLocation(programId, "model"), false, model.getBuffer());
 	}
 }
--- WraithEngine/src/org/wraith/engine/math/Vector3f.java
+++ WraithEngine/src/org/wraith/engine/math/Vector3f.java
@@ -1,173 +1,101 @@
-/*
- * The MIT License (MIT) Copyright © 2015, Heiko Brumme Permission is hereby granted, free of charge, to any person obtaining a copy of this software
- * and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to
- * do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial
- * portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
- * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 package org.wraith.engine.math;
 
 import java.nio.FloatBuffer;
 import org.lwjgl.BufferUtils;
 
-/**
- * This class represents a (x,y,z)-Vector. GLSL equivalent to vec3.
- *
- * @author Heiko Brumme
- */
 public class Vector3f{
+	public static Vector3f subtract(Vector3f left, Vector3f right){
+		return new Vector3f(left.x-right.x, left.y-right.y, left.z-right.z);
+	}
+	public static Vector3f normalize(Vector3f vector){
+		Vector3f n = vector.clone();
+		n.normalize();
+		return n;
+	}
+	public static Vector3f cross(Vector3f left, Vector3f right){
+		Vector3f n = left.clone();
+		n.cross(right);
+		return n;
+	}
+	public static Vector3f lerp(Vector3f a, Vector3f b, float alpha){
+		float i = 1-alpha;
+		float x = a.x*i+b.x*alpha;
+		float y = a.y*i+b.y*alpha;
+		float z = a.z*i+b.z*alpha;
+		return new Vector3f(x, y, z);
+	}
 	public float x;
 	public float y;
 	public float z;
-	/**
-	 * Creates a default 3-tuple vector with all values set to 0.
-	 */
 	public Vector3f(){
-		x = 0f;
-		y = 0f;
-		z = 0f;
+		x = 0.0f;
+		y = 0.0f;
+		z = 0.0f;
 	}
-	/**
-	 * Creates a 3-tuple vector with specified values.
-	 *
-	 * @param x
-	 *            x value
-	 * @param y
-	 *            y value
-	 * @param z
-	 *            z value
-	 */
 	public Vector3f(float x, float y, float z){
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
-	/**
-	 * Adds this vector to another vector.
-	 *
-	 * @param other
-	 *            The other vector
-	 * @return Sum of this + other
-	 */
 	public Vector3f add(Vector3f other){
-		float x = this.x+other.x;
-		float y = this.y+other.y;
-		float z = this.z+other.z;
-		return new Vector3f(x, y, z);
+		x += other.x;
+		y += other.y;
+		z += other.z;
+		return this;
 	}
-	/**
-	 * Calculates the dot product of this vector with another vector.
-	 *
-	 * @param other
-	 *            The other vector
-	 * @return Cross product of this x other
-	 */
 	public Vector3f cross(Vector3f other){
 		float x = y*other.z-z*other.y;
 		float y = z*other.x-this.x*other.z;
 		float z = this.x*other.y-this.y*other.x;
-		return new Vector3f(x, y, z);
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		return this;
 	}
-	/**
-	 * Divides a vector by a scalar.
-	 *
-	 * @param scalar
-	 *            Scalar to multiply
-	 * @return Scalar quotient of this / scalar
-	 */
 	public Vector3f divide(float scalar){
-		return scale(1f/scalar);
+		x /= scalar;
+		y /= scalar;
+		z /= scalar;
+		return this;
 	}
-	/**
-	 * Calculates the dot product of this vector with another vector.
-	 *
-	 * @param other
-	 *            The other vector
-	 * @return Dot product of this * other
-	 */
 	public float dot(Vector3f other){
 		return x*other.x+y*other.y+z*other.z;
 	}
-	/**
-	 * Returns the Buffer representation of this vector.
-	 *
-	 * @return Vector as FloatBuffer
-	 */
 	public FloatBuffer getBuffer(){
 		FloatBuffer buffer = BufferUtils.createFloatBuffer(3);
 		buffer.put(x).put(y).put(z);
 		buffer.flip();
 		return buffer;
 	}
-	/**
-	 * Calculates the length of the vector.
-	 *
-	 * @return Length of this vector
-	 */
 	public float length(){
 		return (float)Math.sqrt(lengthSquared());
 	}
-	/**
-	 * Calculates the squared length of the vector.
-	 *
-	 * @return Squared length of this vector
-	 */
 	public float lengthSquared(){
 		return x*x+y*y+z*z;
 	}
-	/**
-	 * Calculates a linear interpolation between this vector with another vector.
-	 *
-	 * @param other
-	 *            The other vector
-	 * @param alpha
-	 *            The alpha value, must be between 0.0 and 1.0
-	 * @return Linear interpolated vector
-	 */
-	public Vector3f lerp(Vector3f other, float alpha){
-		return scale(1f-alpha).add(other.scale(alpha));
-	}
-	/**
-	 * Negates this vector.
-	 *
-	 * @return Negated vector
-	 */
 	public Vector3f negate(){
-		return scale(-1f);
+		x = -x;
+		y = -y;
+		z = -z;
+		return this;
 	}
-	/**
-	 * Normalizes the vector.
-	 *
-	 * @return Normalized vector
-	 */
 	public Vector3f normalize(){
-		float length = length();
-		return divide(length);
+		divide(length());
+		return this;
 	}
-	/**
-	 * Multiplies a vector by a scalar.
-	 *
-	 * @param scalar
-	 *            Scalar to multiply
-	 * @return Scalar product of this * scalar
-	 */
-	public Vector3f scale(float scalar){
-		float x = this.x*scalar;
-		float y = this.y*scalar;
-		float z = this.z*scalar;
+	public Vector3f multiply(float scalar){
+		x *= scalar;
+		y *= scalar;
+		z *= scalar;
+		return this;
+	}
+	public Vector3f clone(){
 		return new Vector3f(x, y, z);
 	}
-	/**
-	 * Subtracts this vector from another vector.
-	 *
-	 * @param other
-	 *            The other vector
-	 * @return Difference of this - other
-	 */
 	public Vector3f subtract(Vector3f other){
-		return add(other.negate());
+		x -= other.x;
+		y -= other.y;
+		z -= other.z;
+		return this;
 	}
 }
